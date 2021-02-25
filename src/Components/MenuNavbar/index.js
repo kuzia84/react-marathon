06f672ea10fbc5b6f8f09dca372b3a6da524340b
Vasiliy Chumachenco <kuzia84@gmail.com>
2021-02-25 14:14:14 +0200

    HW10

--- src/Components/MenuNavbar/index.js
+++ src/Components/MenuNavbar/index.js
@@ -1,11 +1,16 @@
 import { useState } from "react";
 import Menu from "../Menu";
+import Modal from "../Modal";
 import NavBar from "../NavBar";
 
 const MenuNavbar = ({ bgActive }) => {
   const [isOpen, setOpen] = useState(null);
   const handleClickHamburger = () => {
     setOpen((prevState) => !prevState);
+  };
+  const [isOpenModal, setOpenModal] = useState(false);
+  const handleClickLogin = () => {
+    setOpenModal((prevState) => !prevState);
   };
   return (
     <>
@@ -14,7 +19,15 @@ const MenuNavbar = ({ bgActive }) => {
         isOpen={isOpen}
         bgActive={bgActive}
         onClickHamburger={handleClickHamburger}
+        onClickLogin={handleClickLogin}
       />
+      <Modal
+        title="Log in ..."
+        onCloseModel={handleClickLogin}
+        isOpen={isOpenModal}
+      >
+        Some text here...
+      </Modal>
     </>
   );
 };
---
+++ src/Components/Modal/index.js
@@ -0,0 +1,31 @@
+import { useRef, useEffect } from "react";
+import cn from "classnames";
+import s from "./style.module.css";
+
+const Modal = ({ title, children, onCloseModel, isOpen }) => {
+  useEffect(() => {
+    document.querySelector("body").style.overflow = isOpen ? "hidden" : null;
+  }, [isOpen]);
+  const modalEl = useRef();
+  const handleCloseModal = () => {
+    onCloseModel && onCloseModel(false);
+  };
+  const handleClickRoot = (event) => {
+    if (!modalEl.current.contains(event.target)) {
+      handleCloseModal();
+    }
+  };
+  return (
+    <div className={cn(s.root, { [s.open]: isOpen })} onClick={handleClickRoot}>
+      <div className={s.modal} ref={modalEl}>
+        <div className={s.head}>
+          {title}
+          <span className={s.btnClose} onClick={handleCloseModal}></span>
+        </div>
+        <div className={s.content}>{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
